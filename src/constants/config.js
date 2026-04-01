2e41108e1f0986d8033ed41ccc78ff782fc0bfad
madhavmore23445-saka <madhavmore23445@gmail.com>
2026-04-02 01:24:45 +0530

    first commit

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 // In development on Android emulator, use 10.0.2.2 to connect to localhost on the host machine.
-// On physical device or iOS simulator, use your computer's local network IP address
+// On physical device or iOS simulator, cuse your computer's local network IP address
 // e.g. 'http://192.168.1.5:5001'
 
 const BASE_URL = 'http://10.85.54.48:5001'; // Machine's current local IP
